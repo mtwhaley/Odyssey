98a8a88a868415d4ded6e8caa1c011757f5205ea
mtwhaley <mitchell.t.whaley@gmail.com>
2024-01-23 12:43:31 -0600

    update content

--- SidebarModule/Sidebar.js
+++ SidebarModule/Sidebar.js
@@ -1,4 +1,54 @@
 
+navigation={
+    "home": {
+        sidebarText: "Home",
+        href: "/"
+    },
+    "services": {
+        sidebarText: "Services",
+        href: "Services.html"
+    },
+    "meet": {
+        sidebarText: "Meet your Counselor",
+        href: "Meet.html"
+    },
+    "appointment": {
+        sidebarText: "Request an Appointment",
+        href: "Appointment.html"
+
+    }
+    
+}
+function fillSidebar() {
+    const sidebar=document.getElementById("sidebar")
+    if (sidebar!=null) {
+        const containerDiv=document.createElement("div")
+        containerDiv.setAttribute("class", "navItemContainer")
+        Object.keys(navigation).forEach(function(key) {
+            const object=navigation[key]
+            
+            const anchor=document.createElement("a")
+            const div=document.createElement("div")
+            const button=document.createElement("button")
+
+            div.setAttribute("class", "navItem")
+            anchor.setAttribute("href", object.href)
+            button.setAttribute("class", "navigationButton")
+            button.innerText=object.sidebarText
+            
+            anchor.appendChild(button)
+            
+            containerDiv.appendChild(anchor)
+        })
+
+        sidebar.appendChild(containerDiv)
+
+
+    }
+}
+
+
+
 function toggleSidebar() {
     const sidebar=document.getElementById("sidebarDiv")
     const rect=sidebar.getBoundingClientRect()
@@ -27,3 +77,5 @@ function resetMenuBars(bars) {
         all[i].style.backgroundColor="black"
     }
 }
+
+fillSidebar()
